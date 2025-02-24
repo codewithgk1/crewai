444249630bbcd3e7fdb3fcf9cda07c5a15a6632f
mharkhani <mukesh.harkhani@gmail.com>
2025-02-24 17:42:41 +0530

    flow : or

--- 4_conditional_flows/1_or_flow.py
+++ 4_conditional_flows/1_or_flow.py
@@ -6,12 +6,12 @@ class OrExampleFlow(Flow):
     @start()
     def start_method(self):
         print("Starting flow")
-        return "Hello from the start method"
+        return "Hello from the method 1"
 
     @listen(start_method)
     def second_method(self):
         print("Second method")
-        return "Hello from the second method"
+        return "Hello from the method 2"
 
     @listen(or_(start_method, second_method))
     def logger(self, result):
